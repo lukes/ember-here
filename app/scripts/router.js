340712203321450e5eea1ed22993d1485cd9209e
Luke Duncalfe <lduncalfe@eml.cc>
2014-04-20 21:09:01 +1200

    Returning data from Flickr.
    Photo records being returned using JSONP to cheat the system.

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,4 +1,3 @@
 Here.Router.map(function () {
-  // Add your routes here
   this.resource('photos',{ path : '/' });
 });
--- app/scripts/store.js
+++ app/scripts/store.js
@@ -1,1 +1,25 @@
 Here.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+Here.PhotoAdapter = DS.RESTAdapter.extend({
+
+  findAll: function(store, type, sinceToken) {
+
+    var flickerAPI = "http://api.flickr.com/services/rest?jsoncallback=?";
+
+    return Em.$.ajax({
+      url: flickerAPI,
+      dataType: "jsonp",
+      data: {
+        method: "flickr.people.getPublicPhotos",
+        api_key: "6e83193374827994bec62c2baefffa8c",
+        user_id: "65946368@N00",
+        extras: "geo,media,url_sq",
+        format: "json"
+      }
+    }).then(function(response) {
+      return {photos: response.photos.photo};
+    });
+
+  }
+
+});
