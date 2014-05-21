24940d1e63e13697108b606855abef789209fdc6
Luke Duncalfe <lduncalfe@eml.cc>
2014-05-21 21:32:15 +1200

    Adding foundation grid.

--- app/scripts/views/map_view.js
+++ app/scripts/views/map_view.js
@@ -1,7 +1,7 @@
 Here.MapView = Ember.View.extend({
   didInsertElement: function() {
     // this is required for the map to be rendered
-    this.$().css({ width: "500px", height: "150px" });
+    this.$().css({ width: "100%", height: "150px" });
     // create the LatLng object that will be used to center both the map and the marker
     var center = new google.maps.LatLng(this.get("latitude"),this.get("longitude"));
     // some options
